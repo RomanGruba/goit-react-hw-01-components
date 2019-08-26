2f89d4bd98d629080b130319df69d15cae818bb8
Roman Gruba <roman.gruba@gmail.com>
2019-08-26 16:08:36 +0300

    before adding App

--- src/components/pricing-plan/PricingItem.jsx
+++ src/components/pricing-plan/PricingItem.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Pricing-plan.module.css';
+
+const cx = classNames.bind(styles);
 
 const PricingItem = ({ label, icon, capacity, price, description }) => (
   <div className="pricing-item">
-    <i className={icon} />
+    <i
+      className={cx({
+        icon: true,
+        bronze: label === 'Bronze',
+        silver: label === 'Silver',
+        gold: label === 'Gold',
+      })}
+      style={{ backgroundImage: `url(${icon})` }}
+    />
     <h2 className="label">{label}</h2>
     <p className="capacity">{capacity}</p>
     <p className="description">{description}</p>
     <p className="price">{price}/MO</p>
-    <button className="button">Get Started</button>
+    <button type="button">Get Started</button>
   </div>
 );
 
