ffddefb35a983e906bd920d31125b29ea415acbd
Roman Gruba <roman.gruba@gmail.com>
2019-08-26 13:14:14 +0300

    Updates

---
+++ precache-manifest.8c08df29e76496db049c125ff58a5110.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b080c2106dd01cc4f72f4d0b6924aef",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2913b64aa7c9dec1078d",
+    "url": "/React/static/js/2.ca83f917.chunk.js"
+  },
+  {
+    "revision": "e943e3d7301468c54583",
+    "url": "/React/static/js/main.d3d2c2a9.chunk.js"
+  },
+  {
+    "revision": "21697a9ac3687083dac8",
+    "url": "/React/static/js/runtime~main.dd6060c3.js"
+  }
+]);
