96264cd39aeea26033a02ec760d0a8bf061fa900
Roman Gruba <roman.gruba@gmail.com>
2019-08-26 16:18:54 +0300

    added App

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,38 @@
 import React from 'react';
+import Profile from './social-profile/Profile';
+import Stats from './statistics-section/Stats';
+import pricingPlanItems from './pricing-plan/pricing-plan.json';
+import PricingPlan from './pricing-plan/PricingPlan';
+import transactions from './transaction-history/transactions.json';
+import TransactionHistory from './transaction-history/TransactionHistory';
+
+const user = {
+  name: 'Jacques Gluke',
+  tag: '@jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const stats = [
+  { id: 'id-1', label: '.docx', percentage: 22 },
+  { id: 'id-2', label: '.pdf', percentage: 4 },
+  { id: 'id-3', label: '.mp3', percentage: 17 },
+  { id: 'id-4', label: '.psd', percentage: 47 },
+  { id: 'id-5', label: '.pdf', percentage: 10 },
+];
+
+const App = () => (
+  <>
+    <Profile user={user} />
+    <Stats title="Upload stats" stats={stats} />
+    <PricingPlan items={pricingPlanItems} />
+    <TransactionHistory items={transactions} />
+  </>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Profile from './components/social-profile/Profile';
-import Stats from './components/statistics-section/Stats';
-import pricingPlanItems from './components/pricing-plan/pricing-plan.json';
-import PricingPlan from './components/pricing-plan/PricingPlan';
-import transactions from './components/transaction-history/transactions.json';
-import TransactionHistory from './components/transaction-history/TransactionHistory';
+import App from './components/App';
 
-const user = {
-  name: 'Jacques Gluke',
-  tag: '@jgluke',
-  location: 'Ocho Rios, Jamaica',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
-
-ReactDOM.render(<Profile user={user} />, document.getElementById('root1'));
-
-const stats = [
-  { id: 'id-1', label: '.docx', percentage: 22 },
-  { id: 'id-2', label: '.pdf', percentage: 4 },
-  { id: 'id-3', label: '.mp3', percentage: 17 },
-  { id: 'id-4', label: '.psd', percentage: 47 },
-  { id: 'id-5', label: '.pdf', percentage: 10 },
-];
-
-ReactDOM.render(
-  <Stats title="Upload stats" stats={stats} />,
-  document.getElementById('root2'),
-);
-
-ReactDOM.render(
-  <PricingPlan items={pricingPlanItems} />,
-  document.getElementById('root3'),
-);
-
-ReactDOM.render(
-  <TransactionHistory items={transactions} />,
-  document.getElementById('root4'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
